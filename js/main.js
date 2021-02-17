b27a3d108a068a8d6782a00ca8e2ebf7c2e1cfc0
Noah <noahhodge2@gmail.com>
2021-02-16 16:36:28 -0800

    javascript practice

---
+++ js/main.js
@@ -0,0 +1,9 @@
+function letGo() {
+    let myDiv = document.querySelector(".make-happen")
+    myDiv.style.backgroundColor = "blue"
+}
+
+function makeText() {
+    let myText = document.querySelector(".make-happen p")
+    myText
+}
