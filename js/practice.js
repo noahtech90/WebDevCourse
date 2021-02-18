f7811d5483acd23bada5ee5116eeb4317c4db222
Noah <noahhodge2@gmail.com>
2021-02-17 20:17:49 -0800

    story

--- js/practice.js
+++ js/practice.js
@@ -1,9 +1,13 @@
 function kingdom() {
     let story = document.querySelector("#story")
+    let storyImg = document.querySelector("#story-img")
     story.innerHTML = "You went to the kingdom"
+    storyImg.setAttribute("src") = "../img/kingdom.jfif"
 }
 
 function forest() {
     let story = document.querySelector("#story")
+    let storyImg = document.querySelector("#story-img")
     story.innerHTML = "You went to the forest"
+    storyImg.setAttribute("src") = "../img/forest.jfif"
 }
