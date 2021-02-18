e6d305302579ef0887c2890f9b60e99ba77f7130
Noah <noahhodge2@gmail.com>
2021-02-17 21:14:05 -0800

    wrap

--- js/practice.js
+++ js/practice.js
@@ -2,12 +2,12 @@ function kingdom() {
     let story = document.querySelector("#story")
     let storyImg = document.querySelector("#story-img")
     story.innerHTML = "You went to the kingdom"
-    storyImg.setAttribute("src") = "../img/kingdom.jfif"
+    storyImg.src = "../img/kingdom.jfif"
 }
 
 function forest() {
     let story = document.querySelector("#story")
     let storyImg = document.querySelector("#story-img")
     story.innerHTML = "You went to the forest"
-    storyImg.setAttribute("src") = "../img/forest.jfif"
+    storyImg.src = "../img/forest.jfif"
 }
