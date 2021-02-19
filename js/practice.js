521489b340ea77624806fa2561fc4049a4a64f79
Noah <noahhodge2@gmail.com>
2021-02-18 18:05:50 -0800

    button choice

--- js/practice.js
+++ js/practice.js
@@ -1,6 +1,8 @@
 function kingdom() {
     let story = document.querySelector("#story")
     let storyImg = document.querySelector("#story-img")
+    let btn1 = document.querySelector(".btn"[0])
+    let btn2 = document.querySelector(".btn"[1])
     story.innerHTML = "You went to the kingdom"
     storyImg.src = "../img/kingdom.jfif"
 }
