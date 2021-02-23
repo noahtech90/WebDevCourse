afcd9bc64f46bf3c9a32b43933dd3645a71a68ce
Noah <noahhodge2@gmail.com>
2021-02-22 19:18:25 -0800

    note

--- js/operator.js
+++ js/operator.js
@@ -4,11 +4,7 @@ let z = Math.floor(Math.random() * 255);
 
 document.body.style.backgroundColor = `rgb(${x}, ${y}, ${z})`
 
-let chores = prompt("Done your chores?");
-let reading = prompt("Done your reading?");
-
-if (chores.toLowerCase() === 'yes' || reading.toLowerCase() === 'yes') {
-    alert("alright play your games")
-} else {
-    alert("no games for you")
+let firstName = prompt("enter your first name");
+if (!firstName) {
+    firstName = prompt("try again");
 }
