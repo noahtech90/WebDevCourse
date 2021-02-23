7a92a37f684b14603d4a6c6fd2beb2ab2780ff43
Noah <noahhodge2@gmail.com>
2021-02-22 19:11:22 -0800

    not

--- js/operator.js
+++ js/operator.js
@@ -4,6 +4,11 @@ let z = Math.floor(Math.random() * 255);
 
 document.body.style.backgroundColor = `rgb(${x}, ${y}, ${z})`
 
-if (1 >= 3 && 2 <= 5) {
-    alert("True")
+let chores = prompt("Done your chores?");
+let reading = prompt("Done your reading?");
+
+if (chores.toLowerCase() === 'yes' || reading.toLowerCase() === 'yes') {
+    alert("alright play your games")
+} else {
+    alert("no games for you")
 }
