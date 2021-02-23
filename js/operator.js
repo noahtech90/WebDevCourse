936bcf3e4a8120cc0496be1e48814ee9712426e6
Noah <noahhodge2@gmail.com>
2021-02-22 18:51:28 -0800

    operators

--- js/operator.js
+++ js/operator.js
@@ -1,3 +1,9 @@
+let x = Math.floor(Math.random() * 255);
+let y = Math.floor(Math.random() * 255);
+let z = Math.floor(Math.random() * 255);
 
+document.body.style.backgroundColor = `rgb(${x}, ${y}, ${z})`
 
-document.body.style.backgroundColor = 'green'
+if (1 >= 3 && 2 <= 5) {
+    alert("True")
+}
