14b9cd506c4cdabc523bc4fcd4e60546b7b6d9d5
Noah <noahhodge2@gmail.com>
2021-02-23 19:26:21 -0800

    carousel

---
+++ js/MorePractice.js
@@ -0,0 +1,35 @@
+function turtleArt() {
+    let header = document.getElementById("art-header");
+    let para = document.getElementById("art-descript");
+
+    header.innerHTML = "Turtle Art"
+    para.innerHTML = "This is a photo of a turtle"
+}
+function cactiArt() {
+    let header = document.getElementById("art-header");
+    let para = document.getElementById("art-descript");
+
+    header.innerHTML = "Cacti Art"
+    para.innerHTML = "This is a photo of a cacto"
+}
+function redArt() {
+    let header = document.getElementById("art-header");
+    let para = document.getElementById("art-descript");
+
+    header.innerHTML = "Red House Art"
+    para.innerHTML = "This is a photo of a red house"
+}
+function bridgeArt() {
+    let header = document.getElementById("art-header");
+    let para = document.getElementById("art-descript");
+
+    header.innerHTML = "Bridge Art"
+    para.innerHTML = "This is a photo of a bridge"
+}
+function treeArt() {
+    let header = document.getElementById("art-header");
+    let para = document.getElementById("art-descript");
+
+    header.innerHTML = "Turtle Art"
+    para.innerHTML = "This is a photo of a turtle"
+}
