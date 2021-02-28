142facddf4e6e71a397f63bb3f4fecd8aa1db644
Noah <noahhodge2@gmail.com>
2021-02-28 14:52:56 -0800

    array

--- js/operator.js
+++ js/operator.js
@@ -4,7 +4,4 @@ let z = Math.floor(Math.random() * 255);
 
 document.body.style.backgroundColor = `rgb(${x}, ${y}, ${z})`
 
-let age = parseInt(prompt("enter your first age"));
-if (!age) {
-    firstName = prompt("try again");
-}
+const myArray = ['red', 'blue', 'green']
