61c7f26d069f1e5d3d65733c5d3e92cf16b74680
Noah <noahhodge2@gmail.com>
2021-03-01 13:11:54 -0800

    array

--- js/array.js
+++ js/array.js
@@ -4,8 +4,10 @@ let z = Math.floor(Math.random() * 255);
 
 document.body.style.backgroundColor = `rgb(${x}, ${y}, ${z})`
 
-let todo = [];
-for (i = 0; i <= todo.length; i++) {
-    alert(todo[i])
+let todo = ["hello"];
+let todoList = document.getElementById("todo");
+for (i = 0; i <= todo.length - 1; i++) {
+    let newEle = todoList.appendChild("ul");
+    newEle.innerHTML = todo[i];
 }
 
