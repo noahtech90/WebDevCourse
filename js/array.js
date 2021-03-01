84c5d13c9fac9ffec3095a65e18d0feb0fcfd4e3
Noah <noahhodge2@gmail.com>
2021-03-01 13:14:38 -0800

    array methods

--- js/array.js
+++ js/array.js
@@ -4,10 +4,7 @@ let z = Math.floor(Math.random() * 255);
 
 document.body.style.backgroundColor = `rgb(${x}, ${y}, ${z})`
 
-let todo = ["hello"];
-let todoList = document.getElementById("todo");
-for (i = 0; i <= todo.length - 1; i++) {
-    let newEle = todoList.appendChild("ul");
-    newEle.innerHTML = todo[i];
-}
+let todo = ["hello", 2, 5, 'todo'];
+let todoTwo = ["arrays are kinda boring"]
+alert(todo.concat(todoTwo))
 
