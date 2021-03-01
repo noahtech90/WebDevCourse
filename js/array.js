e29abff59d697c68326029f86a47c1519ccd0825
Noah <noahhodge2@gmail.com>
2021-03-01 13:04:54 -0800

    for loop

--- js/array.js
+++ js/array.js
@@ -4,5 +4,8 @@ let z = Math.floor(Math.random() * 255);
 
 document.body.style.backgroundColor = `rgb(${x}, ${y}, ${z})`
 
-let myArray = ['red', 'blue', 'green'];
-myArray.pop()
+let todo = [];
+for (i = 0; i <= todo.length; i++) {
+    alert(todo[i])
+}
+
