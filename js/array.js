412273826e75aa00bc178c3711f4bc001e583e43
Noah <noahhodge2@gmail.com>
2021-03-03 21:43:52 -0800

    function

--- js/array.js
+++ js/array.js
@@ -1,10 +1,2 @@
-let x = Math.floor(Math.random() * 255);
-let y = Math.floor(Math.random() * 255);
-let z = Math.floor(Math.random() * 255);
-
-document.body.style.backgroundColor = `rgb(${x}, ${y}, ${z})`
-
-let todo = ["hello", 2, 5, 'todo'];
-let todoTwo = ["arrays are kinda boring"]
-alert(todo.slice(2))
-
+function goToParty() {
+}
