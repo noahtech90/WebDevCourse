94dbd359923ababf1cc8a9c3c39a1dcb67d5546d
Noah <noahhodge2@gmail.com>
2021-03-05 17:04:42 -0800

    array memory

--- js/array.js
+++ js/array.js
@@ -1,6 +1,1 @@
-let colors = ['red', 'blue', 'green', 'orange', 'yellow']
-console.log(colors.sort())
-console.log(colors)
-
-let scores = [8, 2, 60, -2, 4000]
-console.log(scores.sort())
+console.log(['hi'] == ['hi'])
