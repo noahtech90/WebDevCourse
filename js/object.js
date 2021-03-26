a91154729597aa7972daf13b2b0aa9d33bfab950
Noah <noahhodge2@gmail.com>
2021-03-26 16:48:12 -0700

    object

--- js/object.js
+++ js/object.js
@@ -3,3 +3,16 @@ const person = {
     totalMiles: 211.7,
     avgCalorieBurn: 2000
 };
+
+const road = {
+    length: 10,
+    person: false
+};
+
+if (person.totalMiles < 1000) {
+    road.person = true
+}
+
+console.log(road)
+
+
