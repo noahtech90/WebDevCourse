20e91ec35192bc1c718c2552a26bf157d084e619
Noah <noahhodge2@gmail.com>
2021-03-26 16:59:53 -0700

    array and object

--- js/arrayAndObject.js
+++ js/arrayAndObject.js
@@ -1,1 +1,9 @@
-console.log(['hi'] == ['hi'])
+const comments = [
+    {
+        username: 'Tammy', text: 'lololol', votes: 9
+    }, {
+        username: 'Saul', text: 'whatcha saying?', votes: 5
+    }
+]
+
+console.log(comments[1].text)
