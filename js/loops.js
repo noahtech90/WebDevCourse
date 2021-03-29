0daa3032dcd1144f98edb1b1371a8a560d8cb86b
Noah <noahhodge2@gmail.com>
2021-03-28 17:00:08 -0700

    nested for loop

--- js/loops.js
+++ js/loops.js
@@ -1,7 +1,7 @@
 const animals = ['lions', 'tigers', 'bears'];
 
 for (let i = 0; i < animals.length; i++) {
-    for (let j = 0; j < i; j++) {
+    for (let j = 0; j < 10; j++) {
         console.log(animals[i], j)
     }
 }
