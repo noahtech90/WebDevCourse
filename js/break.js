c1c3700d9e706a5bbaa65951c0ed4eaa06f3759c
Noah <noahhodge2@gmail.com>
2021-04-06 13:40:26 -0700

    break

--- js/break.js
+++ js/break.js
@@ -1,18 +1,7 @@
-const seatingChart = [
-    ['Mark', 'Eric', 'Candance'],
-    ['Sarah', 'Tony', 'Becaa']
-]
-
-for (i = 0; i < seatingChart.length; i++) {
-    const row = seatingChart[i]
-    console.log(`Row ${i} `)
-    for (j = 0; j < row.length; j++) {
-        console.log(`Person ${row[j]} `)
+let input = prompt("Say something");
+while (true) {
+    input = promt(input);
+    if (input.toLowerCase() === "stop") {
+        break;
     }
-}
-
-count = 0;
-while (count <= 10) {
-    console.log(count);
-    count++;
 }
