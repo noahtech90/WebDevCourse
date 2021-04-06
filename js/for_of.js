21d45e2920c1832fab88adffc762009a847f4175
Noah <noahhodge2@gmail.com>
2021-04-06 14:32:50 -0700

    for of

--- js/for_of.js
+++ js/for_of.js
@@ -3,3 +3,7 @@ const subreddits = ['nature', 'news', 'others'];
 for (let i = 0; i < subreddits.length; i++) {
     console.log(subreddits[i])
 }
+
+for (let sub of subreddits) {
+    console.log(sub)
+}
