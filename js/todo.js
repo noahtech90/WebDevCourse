b123a11331fdee845439b8fd1d53c881eff7edc7
Noah <noahhodge2@gmail.com>
2021-04-20 19:20:23 -0700

    todos

--- js/todo.js
+++ js/todo.js
@@ -1,4 +1,19 @@
 let input = prompt("what would you like to do?");
-while (input != "quit") {
-    input = prompt("what would you like to do?");
+const todos = ["Game", "Win"];
+
+
+while (input != "quit" && input !== "q") {
+    if (input === "list") {
+
+        // List out todos 
+        console.log("*******************")
+        for (let i = 0; i < todos.length; i++) {
+            console.log(`${i}: ${todos[i]}`)
+        }
+        console.log("*******************")
+
+    }
+    input = prompt("what would you like to do?")
 }
+
+todos.push(input)
